0fd1dd0e83599fe53c7098ff3900fe843c67e84b
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-10-19 00:38:10 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -20,4 +20,5 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
