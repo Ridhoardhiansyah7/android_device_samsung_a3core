9da04aae8d1028c4fbc3e66d6cd6d14de2879ac9
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-10-26 17:31:20 +0700

    Update AndroidProducts.mk
    feat(build) : add full support for pbrp_a3core target

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,6 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/twrp_a3core.mk
+    $(LOCAL_DIR)/twrp_a3core.mk \
+    $(LOCAL_DIR)/pbrp_a3core.mk 
 
-COMMON_LUNCH_CHOICES := twrp_a3core-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_a3core-user \
+    twrp_a3core-userdebug \
+    twrp_a3core-eng \
+    pbrp_a3core-user \
+    pbrp_a3core-userdebug \
+    pbrp_a3core-eng
