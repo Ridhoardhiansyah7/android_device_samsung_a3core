a2c2f39797030c29f7ceb45a70fd82a19f495d7b
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-12-16 07:44:01 +0700

    Create load_modules.sh

---
+++ recovery/root/system/bin/load_modules.sh
@@ -0,0 +1,24 @@
+#!/sbin/sh
+
+# Team Hovatek Touch Fix
+load_panel()
+{
+        insmod /vendor/lib/nt36xxx_ss.ko
+        insmod /vendor/lib/chipone-tddi_ss.ko
+        insmod /vendor/lib/chipone-tddi.ko
+        insmod /vendor/lib/gcore_ss.ko
+        insmod /vendor/lib/gcore_7202h.ko
+        insmod /vendor/lib/ilitek-tddi.ko
+        insmod /vendor/lib/focaltech_ts.ko
+        insmod /vendor/lib/tcs3430.ko
+
+        # default
+        chown system system /dev/tspio
+        chown system system /sys/class/sec/tsp/enable
+}
+
+
+load_panel
+wait 1
+setprop modules.loaded 1
+exit 0
