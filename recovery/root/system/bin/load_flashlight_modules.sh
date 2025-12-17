4b4aa3ee3c9e38af02cb87fd9f14025d256e0004
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-12-17 23:22:08 +0700

    Update load_flashlight_modules.sh

--- recovery/root/system/bin/load_flashlight_modules.sh
+++ recovery/root/system/bin/load_flashlight_modules.sh
@@ -3,11 +3,11 @@
 # Load flashlight kernel module
 load_flashlight_module()
 {
-	insmod /system/lib/sprd_flash_drv.ko || return 1
-	insmod /system/lib/flash_ic_sxy378duc.ko || return 1
-  insmod /system/lib/flash_ic_ocp8137.ko || return 1
-  insmod /system/lib/flash_ic_aw3641.ko || return 1
-  insmod /system/lib/flash_ic_sc2703.ko || return 1
+	insmod /vendor/lib/sprd_flash_drv.ko || return 1
+	insmod /vendor/lib/flash_ic_sxy378duc.ko || return 1
+	insmod /vendor/lib/flash_ic_ocp8137.ko || return 1
+	insmod /vendor/lib/flash_ic_aw3641.ko || return 1
+	insmod /vendor/lib/flash_ic_sc2703.ko || return 1
 }
 
 
