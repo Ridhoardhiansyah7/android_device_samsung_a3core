5dd398bc71def3046b2f445b12873e2ed91a28b7
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-12-26 18:23:35 +0700

    Create load_flashlight_modules.sh

---
+++ recovery/root/system/bin/load_flashlight_modules.sh
@@ -0,0 +1,17 @@
+#!/sbin/sh
+
+# Load flashlight kernel module
+load_flashlight_module()
+{
+	insmod /system/lib/modules/sprd_flash_drv.ko || return 1
+	insmod /system/lib/modules/flash_ic_ocp8137.ko || return 1
+  insmod /system/lib/modules/flash_ic_aw3641.ko || return 1
+  insmod /system/lib/modules/flash_ic_sc2703.ko || return 1
+  insmod /system/lib/modules/flash_ic_syx378duc.ko || return 1
+}
+
+
+load_flashlight_module || exit 1
+sleep 1
+setprop modules.loaded 1
+exit 0
